66c26bdd19c541ed7524919b64611f0ebf486066
Shoukuhou Misaki <axmmisaka@users.noreply.github.com>
2022-04-10 21:33:08 -0700

    Remove the need of git blob

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 
 apt-get install -y git python3.8 python3 python3-pip python3-dev
 
-git pull https://github.com/Berkeley-CS170/proj-2022sp-input-autograder.git
+git init
+git remote add origin https://github.com/Berkeley-CS170/proj-2022sp-input-autograder.git
+git pull origin master
 
 python3.8 -m pip install -r /autograder/source/requirements.txt
