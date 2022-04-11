ab3a57e86887ca4c0d0a49fa884e97727a1ec393
Shoukuhou Misaki <axmmisaka@users.noreply.github.com>
2022-04-10 21:30:46 -0700

    Enforce python 3.8

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-apt-get install -y git python3 python3-pip python3-dev
+apt-get install -y git python3.8 python3 python3-pip python3-dev
 
 git pull https://github.com/Berkeley-CS170/proj-2022sp-input-autograder.git
 
-python3 -m pip install -r /autograder/source/requirements.txt
+python3.8 -m pip install -r /autograder/source/requirements.txt
