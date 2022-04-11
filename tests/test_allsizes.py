b00a6dfa072b65698946d4af48592b44a0a53665
Shoukuhou Misaki <axmmisaka@users.noreply.github.com>
2022-04-10 21:41:27 -0700

    Read from gradescope submission dir directly, and changed point values

--- tests/test_allsizes.py
+++ tests/test_allsizes.py
@@ -9,31 +9,30 @@ LARGE_BOUNDS = {"N": (195, 205), "D": (100, 100), "Rs": (3, 3), "Rp": (14, 14)}
 
 class TestInput(unittest.TestCase):
 
-    @gradescope_utils.autograder_utils.decorators.weight(1.0/3.0)
+    @gradescope_utils.autograder_utils.decorators.weight(1.0)
     @gradescope_utils.autograder_utils.decorators.tags("Small")
     def test_small(self):
         try:
-            with open(file=pathlib.Path("./input/small.in"), mode="r") as f:
+            with open(file=pathlib.Path("/autograder/submission/small.in"), mode="r") as f:
                 self.assertTrue(instance.Instance.parse_check_validity(f, SMALL_BOUNDS), "")
         except FileNotFoundError as e:
             self.fail("Could not find small.in.")
     
-    @gradescope_utils.autograder_utils.decorators.weight(1.0/3.0)
+    @gradescope_utils.autograder_utils.decorators.weight(1.0)
     @gradescope_utils.autograder_utils.decorators.tags("Medium")
     def test_medium(self):
         try:
-            with open(file=pathlib.Path("./input/medium.in"), mode="r") as f:
+            with open(file=pathlib.Path("/autograder/submission/medium.in"), mode="r") as f:
                 self.assertTrue(instance.Instance.parse_check_validity(f, MEDIUM_BOUNDS), "")
         except FileNotFoundError as e:
             self.fail("Could not find medium.in.")
 
-    @gradescope_utils.autograder_utils.decorators.weight(1.0/3.0)
+    @gradescope_utils.autograder_utils.decorators.weight(1.0)
     @gradescope_utils.autograder_utils.decorators.tags("Large")
     def test_large(self):
         try:
-            with open(file=pathlib.Path("./input/large.in"), mode="r") as f:
+            with open(file=pathlib.Path("/autograder/submission/large.in"), mode="r") as f:
                 self.assertTrue(instance.Instance.parse_check_validity(f, LARGE_BOUNDS), "")
         except FileNotFoundError as e:
             self.fail("Could not find large.in.")
     
-
